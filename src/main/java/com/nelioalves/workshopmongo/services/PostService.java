77bb8e116e18236d8262ab9c2abacee43da777a1
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-02-03 19:35:10 -0300

    Consultas simples com Query Methods

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +22,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	// metodo de buca
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
